9aa73b1e8eccfc2497e6f48bef1f46e837b32f2e
longhongwei <ZXmCl@bitunix.io>
2024-08-01 14:50:02 +0800

    update validator

--- src/main/java/com/bitunix/model/MerkleTreePath.java
+++ src/main/java/com/bitunix/model/MerkleTreePath.java
@@ -35,12 +35,9 @@ public class MerkleTreePath {
         TreeNode newRoot = new MerKelTree().buildMerkelTreeRoot(path, self, self.getPrecisions());
         TreeNode oldRoot = path.get(path.size() - 1);
 
-        System.out.printf("Generated Root BTC balance : %s, merkel_tree_path Root BTC balance in file: %s%n", newRoot.getBalances().get("BTC"), oldRoot.getBalances().get("BTC"));
-        System.out.printf("Generated Root ETH balance : %s, merkel_tree_path Root ETH balance in file: %s%n", newRoot.getBalances().get("ETH"), oldRoot.getBalances().get("ETH"));
-        System.out.printf("Generated Root USDT balance : %s, merkel_tree_path Root USDT balance in file: %s%n", newRoot.getBalances().get("USDT"), oldRoot.getBalances().get("USDT"));
         System.out.printf("Generated Root MerkelLeaf : %s, merkel_tree_path Root MerkelLeaf in file: %s%n", newRoot.getMerkleLeaf(), oldRoot.getMerkleLeaf());
 
-        return newRoot.getMerkleLeaf().equals(oldRoot.getMerkleLeaf()) && newRoot.validateEqualsBalances(oldRoot) && newRoot.getHeight().equals(oldRoot.getHeight());
+        return newRoot.getMerkleLeaf().equals(oldRoot.getMerkleLeaf());
     }
 
 }
